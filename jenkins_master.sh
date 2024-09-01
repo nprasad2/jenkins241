3f7420ef31a9af6d39af603682f5c2dea995fff0
hkaranam <hkaranam241@gmail.com>
2024-09-01 06:46:01 -0700

    first commit

--- jenkins_master.sh
+++ jenkins_master.sh
@@ -24,7 +24,7 @@ validate(){
 }
 
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo --secure-protocol tlsv1
 validate $? "creating jenkins.repo"
 
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
