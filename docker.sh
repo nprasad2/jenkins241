3a16c6dd719d05ca57b929a0988634438b804aeb
hkaranam <hkaranam241@gmail.com>
2024-09-08 11:30:20 -0700

    first commit

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then 
+    echo " You are in su mode and can run the script now"
+else 
+    echo " please run the script in su mode"
+fi 
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+ 
+sudo systemctl start docker
+
+ sudo useradd -g docker docker
